f0dcdbf7f39b176a0dfa60b7fdfeb50acc783161
Nitin Joshi <njoshi023@gmail.com>
2013-09-08 17:43:17 -0400

    new URL router

--- yelpapiserver/urls.py
+++ yelpapiserver/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'yelpapiserver.views.home', name='home'),
+    url(r'^$', 'yelpapiserver.views.home', name='home'),
     # url(r'^yelpapiserver/', include('yelpapiserver.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
